664677266ac0e85d649da7791c2be87bff3a3b15
xuwenyihust <1016635786@qq.com>
2017-10-05 16:46:27 -0500

    Add log format checking.

--- src/main/scala/logagent/LogAgent.scala
+++ src/main/scala/logagent/LogAgent.scala
@@ -5,19 +5,40 @@ import scala.sys.process._
 object LogAgent extends App {
   println("==================\nThis is log agent\n==================")
 
-  def someProcessing(line: String): Unit = {
+  def streamProcessing(line: String): Unit = {
     // do whatever you want with that line
     print("[just read this line] ")
     println(line)
+
+    val lineFormat = checkFormat(line)
+  }
+
+  def checkFormat(line: String): String = {
+    // Apache Access Pattern
+    val apacheAccessIp = "[0-9]+.[0-9]+.[0-9]+.[0-9]+"
+    val apacheAccessRfc = "-"
+    val apacheAccessUid = "[a-zA-Z]+"
+    val apacheAccessTime = "\\[[0-9]+\\/[a-zA-Z]+\\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} -[0-9]{4}\\]"
+    val apacheAccessMsg = ""
+    val apacheAccessHttp = ""
+    val apacheAccessSize = ""
   }
 
   // the file to read
   val file = "test.log"
 
+  //val source = Source.fromFile(file)
+  //val firstLine = source.getLines().toList.head
+
   // the process to start
   val tail = Seq("tail", "-f", file)
 
-  // continuously read lines from tail -f
-  tail.lineStream.foreach(someProcessing)
+  try {
+    // continuously read lines from tail -f
+    tail.lineStream.foreach(streamProcessing)
+  }
+  catch {
+    case e: RuntimeException => println("Error! Cannot collect logs from the given file name!")
+  }
   // careful: this never returns (unless tail is externally killed)
 }
