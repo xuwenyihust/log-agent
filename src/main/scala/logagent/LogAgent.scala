97b633c700069efae6e000e3f6526d4a914722f4
xuwenyihust <1016635786@qq.com>
2017-10-10 13:31:23 -0500

    Some updates.

--- src/main/scala/logagent/LogAgent.scala
+++ src/main/scala/logagent/LogAgent.scala
@@ -7,22 +7,32 @@ object LogAgent extends App {
 
   def streamProcessing(line: String): Unit = {
     // do whatever you want with that line
-    print("[just read this line] ")
+    print("[Just read this line] ")
     println(line)
 
     val lineFormat = checkFormat(line)
+    print("[Check the line format] ")
+    println(lineFormat)
   }
 
-  def checkFormat(line: String): String = {
-    // Apache Access Pattern
-    val apacheAccessIp = "[0-9]+.[0-9]+.[0-9]+.[0-9]+"
-    val apacheAccessRfc = "-"
-    val apacheAccessUid = "[a-zA-Z]+"
-    val apacheAccessTime = "\\[[0-9]+\\/[a-zA-Z]+\\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} -[0-9]{4}\\]"
-    val apacheAccessMsg = ""
-    val apacheAccessHttp = ""
-    val apacheAccessSize = ""
+  // Apache Access Line Fields Pattern
+  // 127.0.0.1 user-identifier frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326
+  val apacheAccessIp = "[0-9]+.[0-9]+.[0-9]+.[0-9]+"
+  val apacheAccessRfc = "-"
+  val apacheAccessUid = "[a-zA-Z]+"
+  val apacheAccessTime = "\\[[0-9]+\\/[a-zA-Z]+\\/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} -[0-9]{4}\\]"
+  val apacheAccessMsg = "\"[0-9a-zA-Z./_ ]+\""
+  val apacheAccessHttp = "[0-9]{3}"
+  val apacheAccessSize = "[0-9]+"
+  // Apache Access Line Pattern
+  val apacheAccess = "%s %s %s %s %s %s %s".format(apacheAccessIp, apacheAccessRfc, apacheAccessUid, apacheAccessTime, apacheAccessMsg, apacheAccessHttp, apacheAccessSize)
+
+
+  def checkFormat(line: String): String = line match {
+    case l if l.matches(apacheAccess) => "Apache access log."
+    case _ => "Not recognized."
   }
+
 
   // the file to read
   val file = "test.log"
